d16af11979831e235de3908d358b26e72f8390af
Peter Gray <peter@petergraycreative.com>
2018-05-05 16:35:34 -0500

    Try out sprintf as alternative to concatting strings

--- functions.php
+++ functions.php
@@ -16,43 +16,36 @@ add_action( 'wp_enqueue_scripts', 'add_stylesheet' );
 
 function news_link_shortcode($atts)
 {
-  $newsLinksLoop;
-  // Set up item variables to make them easier to output inside strings
-  // $timeDiff = $currentTime - $postTime;
+  $output;
   $links = new WP_Query(array('post_type' => 'news_posts'));
 
   if ($links->have_posts()) {
     while ($links->have_posts()) : $links->the_post();
-    $newsLinksLoop .= '<div class="panel-news_article">';
+    $output .= sprintf('<div class="panel-news_article">');
     if (has_post_thumbnail()) {
 
-      $newsLinksLoop .= '<div class="news-featured"><a href="';
-      $newsLinksLoop .= the_permalink();
-      $newsLinksLoop .= '">';
-      $newsLinksLoop .= the_post_thumbnail();
-      $newsLinksLoop .= '</a>
-      </div>';
+      $output .= sprintf('<div class="news-featured"><a href="%s">%s</a></div>', the_permalink(), the_post_thumbnail());
     }
-    $newsLinksLoop .=
+    $output .=
       '<div class="title">
                     <h2>';
-    $newsLinksLoop .= get_the_title();
-    $newsLinksLoop .= '</h2>
+    $output .= get_the_title();
+    $output .= '</h2>
                 </div>
                 <div class="meta">
                     <span>';
-    $newsLinksLoop .= get_field('news_outlet');
-    $newsLinksLoop .= '</span>
+    $output .= get_field('news_outlet');
+    $output .= '</span>
                     <span>Time Since Posted: ';
-    $newsLinksLoop .= the_field('post_date');
-    $newsLinksLoop .= '</span>
+    $output .= the_field('post_date');
+    $output .= '</span>
                 </div>
             </div>';
 
     endwhile;
   }
   wp_reset_postdata();
-  return $newsLinksLoop;
+  return $output;
 }
 add_shortcode('news-links', 'news_link_shortcode');
 ?>

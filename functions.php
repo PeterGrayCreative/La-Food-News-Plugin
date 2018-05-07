17e27b682ecf775cba214c6751ae8b1dea7d6233
Peter Gray <peter@petergraycreative.com>
2018-05-06 22:06:57 -0500

    Add button shortcode

--- functions.php
+++ functions.php
@@ -46,9 +46,11 @@ function news_link_shortcode($atts)
     if (has_post_thumbnail()) {
       $output .= sprintf('<div class="news-featured"><a href="%s">%s</a></div>', the_permalink(), the_post_thumbnail());
     }
+    $article_link = get_field('article_link');
+    $btn = do_shortcode('[boombox_button url="' . $article_link . '" tag_type="button" size="small"]' . 'Small button' . '[/boombox_button]');
     $isNewPost = is_new_item(get_post_time('U', 'gmt', get_the_ID())) ? ' new-link' : '';
-    $output .= sprintf('<div class="title"><a href="%s"><h2>%s<span class="label new">%s</span></h2></a></div>', get_field('article_link'), get_the_title(), ($isNewPost ? 'new' : ''));
-    $output .= sprintf('<a href="" class="summary-link btn">Read Summary</a><p class="summary display-none">%s</p>', strip_tags(get_the_excerpt()));
+    $output .= sprintf('<div class="title"><a href="%s"><h2>%s<span class="label new">%s</span></h2></a></div>', $article_link, get_the_title(), ($isNewPost ? 'new' : ''));
+    $output .= sprintf('<a href="" class="summary-link btn">Read Summary</a><p class="summary display-none">%s %s</p>', strip_tags(get_the_excerpt()), $btn);
     $output .= sprintf('<div class="meta"><span>%s</span>', get_field('news_outlet'));
     $output .= sprintf('<span>%s</span></div></div>', time_since_post(get_post_time('U', 'gmt', get_the_ID())));
     endwhile;

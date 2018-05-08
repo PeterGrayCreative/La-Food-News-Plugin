c5f5807f6210653e4d128fd1b882784f999c7758
Peter Gray <peter@petergraycreative.com>
2018-05-07 19:46:21 -0500

    Remove link all together

--- functions.php
+++ functions.php
@@ -63,7 +63,7 @@ function news_link_shortcode($atts)
     $base_url = $rootLink[0];
     $btn = do_shortcode('[boombox_button url="' . $article_link . '" tag_type="a" size="small" type="primary"]' . 'Read Full Article' . '[/boombox_button]');
     $isNewPost = is_new_item(get_post_time('U', 'gmt', get_the_ID())) ? ' new-link' : '';
-    $output .= sprintf('<div class="title"><a href=""><span class="news-title">%s<span class="label new">%s</span></span></a></div>', get_the_title(), ($isNewPost ? 'new' : ''));
+    $output .= sprintf('<div class="title"><span class="news-title">%s<span class="label new">%s</span></span></div>', get_the_title(), ($isNewPost ? 'new' : ''));
     $output .= sprintf('<div class="summary display-none"><p>%s</p>%s</div>', strip_tags(get_the_excerpt()), $btn);
     $output .= sprintf('<div class="meta"><a href="%s"><span>%s</span></a>', $base_url, get_field('news_outlet'));
     $output .= sprintf('<span>&#183</span><span>%s</span></div></div>', time_since_post(get_post_time('U', 'gmt', get_the_ID())));

86215faa8768a3c8cc135b458c1dec104a14dc4c
Peter Gray <peter@petergraycreative.com>
2018-05-11 22:12:17 -0500

    Catch regex errors

--- functions.php
+++ functions.php
@@ -59,13 +59,17 @@ function news_link_shortcode($atts)
       $output .= sprintf('<div class="news-featured"><a href="%s">%s</a></div>', the_permalink(), the_post_thumbnail());
     }
     $article_link = get_field('article_link');
+    try {
     preg_match('/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,8})*\//', $article_link, $rootLink);
     $base_url = $rootLink[0];
+    } catch (Exception $err) {
+      printf('Error, %s', $err->getMessage());
+    }
     $btn = do_shortcode('[boombox_button url="' . $article_link . '" tag_type="a" size="small" type="primary"]' . 'Read Full Article' . '[/boombox_button]');
     $isNewPost = is_new_item(get_post_time('U', 'gmt', get_the_ID())) ? ' new-link' : '';
     $output .= sprintf('<div class="title"><span class="news-title">%s<span class="label %s">%s</span></span></div>', get_the_title(), ($isNewPost ? 'new' : ''), ($isNewPost ? 'new' : ''));
     $output .= sprintf('<div class="summary display-none"><p>%s</p>%s</div>', strip_tags(get_the_excerpt()), $btn);
-    $output .= sprintf('<div class="meta"><a href="%s"><span>%s</span></a>', $base_url, get_field('news_outlet'));
+    $output .= sprintf('<div class="meta"><a href="%s"><span>%s</span></a>', $base_url ? $base_url : '', get_field('news_outlet'));
     $output .= sprintf('<span>&#183</span><span>%s</span></div></div>', time_since_post(get_post_time('U', 'gmt', get_the_ID())));
     endwhile;
   }

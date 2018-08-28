60aae31128bf54e06539fc8ee4cd1e2358147307
Peter Gray <peter@petergraycreative.com>
2018-08-27 22:29:55 -0500

    Add echo out query results

--- functions.php
+++ functions.php
@@ -75,6 +75,7 @@ function news_link_shortcode($atts)
     ),
   );
   $links = new WP_Query($args);
+  echo $links;
   $output = '';
   if ($links->have_posts()) {
     while ($links->have_posts()) : $links->the_post();

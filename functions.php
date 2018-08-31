9f60eceb557e83d0b4bb3f1110bdd106a8d58420
Peter Gray <peter@petergraycreative.com>
2018-08-30 22:04:56 -0500

    Debug news list

--- functions.php
+++ functions.php
@@ -61,7 +61,7 @@ function time_since_post($postTime)
 function news_link_shortcode($atts)
 {
   $args = array(
-    'post_type' => 'news',
+    'post_type' => 'news_posts',
     'tax_query' => array(
       array(
         'label' => 'News',

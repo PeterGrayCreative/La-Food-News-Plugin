fb0fe7fea832069c4a7b992b757a80829b9e55b6
Peter Gray <peter@petergraycreative.com>
2018-09-01 18:30:01 -0500

    Update news categories

--- functions.php
+++ functions.php
@@ -62,6 +62,38 @@ function custom_news_post_type() {
 }
 add_action( 'init', 'custom_news_post_type', 0 );
 
+add_action( 'init', 'create_news_cat_taxonomies', 0 );
+
+function create_news_cat_taxonomies() 
+{
+  $labels = array(
+    'name' => _x( 'Categories', 'taxonomy general name' ),
+    'singular_name' => _x( 'Category', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Categories' ),
+    'popular_items' => __( 'Popular Categories' ),
+    'all_items' => __( 'All Categories' ),
+    'parent_item' => null,
+    'parent_item_colon' => null,
+    'edit_item' => __( 'Edit Category' ), 
+    'update_item' => __( 'Update Category' ),
+    'add_new_item' => __( 'Add New Category' ),
+    'new_item_name' => __( 'New Category Name' ),
+    'separate_items_with_commas' => __( 'Separate tags with commas' ),
+    'add_or_remove_items' => __( 'Add or remove tags' ),
+    'choose_from_most_used' => __( 'Choose from the most used tags' ),
+    'menu_name' => __( 'News Cats' ),
+  ); 
+
+  register_taxonomy('news_category','news',array(
+    'hierarchical' => false,
+    'labels' => $labels,
+    'show_ui' => true,
+    'update_count_callback' => '_update_post_term_count',
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'news-category' ),
+  ));
+}
+
 add_action( 'init', 'create_news_tag_taxonomies', 0 );
 // Add custom tag taxonomy for News Links
 function create_news_tag_taxonomies() 
@@ -94,37 +126,6 @@ function create_news_tag_taxonomies()
   ));
 }
 
-add_action( 'init', 'create_news_cat_taxonomies', 0 );
-
-function create_news_cat_taxonomies() 
-{
-  $labels = array(
-    'name' => _x( 'Categories', 'taxonomy general name' ),
-    'singular_name' => _x( 'Category', 'taxonomy singular name' ),
-    'search_items' =>  __( 'Search Categories' ),
-    'popular_items' => __( 'Popular Categories' ),
-    'all_items' => __( 'All Categories' ),
-    'parent_item' => null,
-    'parent_item_colon' => null,
-    'edit_item' => __( 'Edit Category' ), 
-    'update_item' => __( 'Update Category' ),
-    'add_new_item' => __( 'Add New Category' ),
-    'new_item_name' => __( 'New Category Name' ),
-    'separate_items_with_commas' => __( 'Separate tags with commas' ),
-    'add_or_remove_items' => __( 'Add or remove tags' ),
-    'choose_from_most_used' => __( 'Choose from the most used tags' ),
-    'menu_name' => __( 'News Cats' ),
-  ); 
-
-  register_taxonomy('news_category','news',array(
-    'hierarchical' => false,
-    'labels' => $labels,
-    'show_ui' => true,
-    'update_count_callback' => '_update_post_term_count',
-    'query_var' => true,
-    'rewrite' => array( 'slug' => 'news-category' ),
-  ));
-}
 
 function init_plugin()
 {

9d6f1e3133ab78e6747f7138f56d2e2eb98db6db
Peter Gray <peter@petergraycreative.com>
2018-09-01 21:20:19 -0500

    Delete extra tag code

--- functions.php
+++ functions.php
@@ -113,45 +113,6 @@ function custom_news_post_type()
 }
 add_action('init', 'custom_news_post_type', 0);
 
-add_action('init', 'create_news_cat_taxonomies', 0);
-
-function create_news_cat_taxonomies()
-{
-
-}
-
-add_action('init', 'create_news_tag_taxonomies', 0);
-// Add custom tag taxonomy for News Links
-function create_news_tag_taxonomies()
-{
-  $labels = array(
-    'name' => _x('News Tags', 'taxonomy general name'),
-    'singular_name' => _x('Tag', 'taxonomy singular name'),
-    'search_items' => __('Search Tags'),
-    'popular_items' => __('Popular Tags'),
-    'all_items' => __('All Tags'),
-    'parent_item' => null,
-    'parent_item_colon' => null,
-    'edit_item' => __('Edit Tag'),
-    'update_item' => __('Update Tag'),
-    'add_new_item' => __('Add New Tag'),
-    'new_item_name' => __('New Tag Name'),
-    'separate_items_with_commas' => __('Separate tags with commas'),
-    'add_or_remove_items' => __('Add or remove tags'),
-    'choose_from_most_used' => __('Choose from the most used tags'),
-    'menu_name' => __('News Tags'),
-  );
-
-  register_taxonomy('news_tag', 'news', array(
-    'hierarchical' => false,
-    'labels' => $labels,
-    'show_ui' => true,
-    'update_count_callback' => '_update_post_term_count',
-    'query_var' => true,
-    'rewrite' => array('slug' => 'news-tag'),
-  ));
-}
-
 
 function init_plugin()
 {
